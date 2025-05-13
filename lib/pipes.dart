2fd58273f63485ca38adecf67c71929ba66a3a03
mrone <mroneill30@gmail.com>
2025-05-12 17:30:31 -0700

    pipes

---
+++ lib/pipes.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyPipes extends StatelessWidget {
+  final size;
+  MyPipes({this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: size,
+      decoration: BoxDecoration(
+          color: Colors.greenAccent,
+          border: Border.all(width: 10, color: Colors.greenAccent.shade700),
+          borderRadius: BorderRadius.circular(15)),
+    );
+  }
+}
